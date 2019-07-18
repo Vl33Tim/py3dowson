3f8e5ed408b78e1d977c037b75d8f511e8711bb5
Vl33Tim <vladyslav.t@i.ua>
2019-07-18 15:56:06 +0300

    6.2 fixed, 6.3 added

--- chapter06/6.3main_func_guess_numb.py
+++ chapter06/6.3main_func_guess_numb.py
@@ -11,12 +11,8 @@ print("\tWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and 100.")
 print("Try to guess it in as few attempts as possible.\n")
 
-# set the initial values
-the_number = random.randint(1, 100)
-guess = None
-tries = 1
-top = 100
-bot = 1
+
+
 
 def ask_number(question = "Take a guess beetween (1 - 100):",low = 1, high = 100,step = 1):
     """Ask for a number within a range."""
@@ -25,22 +21,30 @@ def ask_number(question = "Take a guess beetween (1 - 100):",low = 1, high = 100
         response = int(input(question))
     return response
 
-# guessing loop
-while guess != the_number:
-    guess = ask_number(low = bot, high = top)
-    if guess > the_number:
-        top = guess
-        print("Lower...")
-        print("Between {} and {}".format(bot,top))
-    else:
-        bot = guess
-        print("Higher...")
-        print("Between {} and {}".format(bot,top))
+def main():
+# set the initial values
+    the_number = random.randint(1, 100)
+    guess = None
+    tries = 1
+    top = 100
+    bot = 1
+    while guess != the_number:
+        guess = ask_number(low = bot, high = top)
+        if guess > the_number:
+            top = guess
+            print("Lower...")
+            print("Between {} and {}".format(bot,top))
+        else:
+            bot = guess
+            print("Higher...")
+            print("Between {} and {}".format(bot,top))
             
     #guess = int(input("Take a guess: "))
-    tries += 1
+        tries += 1
 
-print("You guessed it!  The number was", the_number)
-print("And it only took you", tries, "tries!\n")
-  
+    print("You guessed it!  The number was", the_number)
+    print("And it only took you", tries, "tries!\n")
+
+
+main()
 input("\n\nPress the enter key to exit.")
